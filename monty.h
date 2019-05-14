7abd04b0a28eb8adfc16bfc61001a25a7c4a7075
snehadasa <sneha.dasasneha@gmail.com>
2019-05-14 14:52:25 -0700

    headers

--- monty.h
+++ monty.h
@@ -36,5 +36,8 @@ typedef struct instruction_s
 } instruction_t;
 
 extern int n;
+void pall(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+
 
 #endif
