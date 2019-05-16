cb0df4b63f33b4f89ea6f0a05e10768dc488d7a8
snehadasa <sneha.dasasneha@gmail.com>
2019-05-15 20:46:24 -0700

    betty errors

--- monty.h
+++ monty.h
@@ -34,8 +34,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 extern int n;
