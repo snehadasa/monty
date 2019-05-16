669077190beaafa5459b0dd7e0d1d81fce13bc76
snehadasa <sneha.dasasneha@gmail.com>
2019-05-15 20:38:09 -0700

    adding untracked files

---
+++ div.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * div - prints elements present in the list.
+ * @stack: list to be printed
+ * @line_number: the current pointing line
+ * Return: elements in the list "h".
+ */
+void _div(stack_t **stack, unsigned int line_number)
+{
+	int result = 0;
+
+	if (!stack || !(*stack) || !(*stack)->next)
+	{
+		dprintf(STDERR_FILENO, "L%u: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	result = ((*stack)->next)->n / (*stack)->n;
+
+	((*stack)->next)->n = result;
+
+	free(*stack);
+
+	pop(stack, line_number);
+}
---
+++ mul.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * mul - prints elements present in the list.
+ * @stack: list to be printed
+ * @line_number: the current pointing line
+ * Return: elements in the list "h".
+ */
+void mul(stack_t **stack, unsigned int line_number)
+{
+	int result = 0;
+
+	if (!stack || !(*stack) || !(*stack)->next)
+	{
+		dprintf(STDERR_FILENO, "L%u: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	result = (*stack)->n * ((*stack)->next)->n;
+
+	((*stack)->next)->n = result;
+
+	free(*stack);
+
+	pop(stack, line_number);
+}
