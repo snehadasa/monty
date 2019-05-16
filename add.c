5548aaa8649f2fbf44677e65b9673b7bb9c1d735
snehadasa <sneha.dasasneha@gmail.com>
2019-05-15 18:41:12 -0700

    add

---
+++ add.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * add - prints elements present in the list.
+ * @stack: list to be printed
+ * @line_number: the current pointing line
+ * Return: elements in the list "h".
+ */
+void add(stack_t **stack, __attribute__((unused)) unsigned int line_number)
+{
+	int sum = 0;
+
+	if (!stack || !(*stack) || !(*stack)->next)
+	{
+		printf("L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	sum = (*stack)->n + ((*stack)->next)->n;
+
+	((*stack)->next)->n = sum;
+
+	free(*stack);
+
+	pop(stack, line_number);
+}
